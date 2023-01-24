52e3d850bc57d4567b0d9ffbfd44b8c5570d17b9
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2023-01-24 19:33:11 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nuvens Goiânia - Guia de Identificação
+
+Este aplicativo tem por objetivo ajudar a identificar e prever os fenômenos climáticos com base na observação das nuvens.
---
+++ android/app/src/main/java/com/wanderalvess/clouds_identification_tab/MainActivity.java
@@ -0,0 +1,6 @@
+package com.wanderalvess.clouds_identification_tab;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
