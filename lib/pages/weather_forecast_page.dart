0d2091ed0ccec072ea3a37fea148915e589b9831
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2024-02-27 16:10:27 -0300

    Criação do card para previsão do tempo

--- lib/pages/weather_forecast_page.dart
+++ lib/pages/weather_forecast_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:clouds_identification_tab/model/my_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
@@ -11,34 +12,46 @@ class WeatherForecastPage extends StatefulWidget {
 class _WeatherForecastState extends State<WeatherForecastPage> {
   TextEditingController _cityController = TextEditingController();
   String _city = '';
-  String _temp = '';
+  String _temp = ''; //temperatura atual
+  String _description = ''; //descrição do tempo
+  String _cloudiness = ''; //nebulosidade
+  String _rain = ''; //volume de chuva
+  String _currently = ''; //dia ou noite
+  String _wind_speedy = ''; //velocidade vento
+  String _wind_cardinal = ''; //rosa dos ventos
+  String _condition_slug = ''; //imagem da condição do tempo,
+  String _weekday = ''; //dia da semana
+  String _date = ''; //data
   String _temp_min = '';
   String _temp_max = '';
   String _humidity = '';
-  String _pressure = '';
+  String _image_url = '';
 
   Future<void> _getWeatherForecast(String city) async {
-    final apikey = '90e3cc1e43979f2c222283e3a78f6126';
     final apikeyHG = '14193a96';
-    final apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apikey&units=metric';
-    final apiUrlHG = 'https://api.hgbrasil.com/weather?key=$apikeyHG&city_name=$city';
-    final response = await http.get(Uri.parse(apiUrl));
+    final apiUrlHG =
+        'https://api.hgbrasil.com/weather?key=$apikeyHG&city_name=$city';
+    final responseHG = await http.get(Uri.parse(apiUrlHG));
 
-
-
-
-   // if do openweather
-
-    if (response.statusCode == 200) {
-      final jsonData = jsonDecode(response.body);
+    if (responseHG.statusCode == 200) {
+      final jsonData = jsonDecode(responseHG.body);
       setState(() {
-        _city = jsonData['name'];
-        _temp = jsonData['main']['temp'].toString();
-        _temp_max = jsonData['main']['temp_max'].toString();
-        _temp_min = jsonData['main']['temp_min'].toString();
-        _pressure = jsonData['main']['pressure'].toString();
-        _humidity = jsonData['main']['humidity'].toString();
+        _city = jsonData['results']['city'];
+        _temp = jsonData['results']['temp'].toString();
+        _temp_max = jsonData['results']['forecast'][0]['max'].toString();
+        _temp_min = jsonData['results']['forecast'][0]['min'].toString();
+        _weekday = jsonData['results']['forecast'][0]['weekday'];
+        _date = jsonData['results']['forecast'][0]['date'].toString();
+        _humidity = jsonData['results']['humidity'].toString();
+        _description = jsonData['results']['description'];
+        _cloudiness = jsonData['results']['cloudiness'].toString();
+        _rain = jsonData['results']['rain'].toString();
+        _currently = jsonData['results']['currently'];
+        _wind_speedy = jsonData['results']['wind_speedy'].toString();
+        _wind_cardinal = jsonData['results']['wind_cardinal'].toString();
+        _condition_slug = jsonData['results']['condition_slug'];
       });
+      getImageWeatherUrls(_condition_slug);
     } else {
       setState(() {
         _city = 'Erro ao obter a cidade';
@@ -47,23 +60,36 @@ class _WeatherForecastState extends State<WeatherForecastPage> {
     }
   }
 
+  Future<void> getImageWeatherUrls(String conditionSlug) async {
+    final response = await http.get(Uri.parse(
+        'https://back-app-clouds.onrender.com/api/image/images/weather?name=$conditionSlug'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      setState(() {
+        _image_url = data[0]['imageUrl'].toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Previsão do tempo'),
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          centerTitle: true,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+      appBar: AppBar(
+        title: const Text('Previsão do tempo'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -77,17 +103,113 @@ class _WeatherForecastState extends State<WeatherForecastPage> {
               ElevatedButton(
                 onPressed: () {
                   _getWeatherForecast(_cityController.text);
-                }, child: Text('Consultar'),
+                },
+                child: Text('Consultar'),
               ),
               SizedBox(height: 16.0),
-              Text(
-                'Temperatura para $_city: é $_temp °C',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              )
+              Visibility(
+                visible: _city != null && _city.isNotEmpty,
+                child: Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _city,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              _date,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Temperatura',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      '$_temp°C',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(width: 16), // Adiciona um espaçamento entre as colunas
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Nebulosidade',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      _cloudiness.replaceAll('.', ',') + '%',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            const Text(
+                              'Descrição do Tempo:',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              _description,
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(), // Adiciona um espaço flexível entre as colunas
+                        Image.network(
+                          _image_url,
+                          height: 100,
+                          scale: 1.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
-        )
+        ),
+      ),
     );
   }
 }
